9b85e5a600f9ad72970c99f24567f987661775b9
Felipe Ramos <pipoblakgamer@gmail.com>
2018-11-10 22:19:28 -0200

    UserResource

--- routes/users_resources.js
+++ routes/users_resources.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var app = express();
 router.patch('/update_permission', function (req, res) {
   sequelize.sync()
-    .then(() => model.update({active: req.body.permission},{ where: {user_id:req.body.user_id, resource_id: req.body.resource_id} }))
+    .then(() => UserResource.update({active: req.body.permission},{ where: {user_id:req.body.user_id, resource_id: req.body.resource_id} }))
     .then(user_resource => {
       res.send(user_resource);
   }).catch(function (err) {
@@ -12,7 +12,7 @@ router.patch('/update_permission', function (req, res) {
 });
 router.get('/has_permission', function (req, res) {
   sequelize.sync()
-    .then(() => model.find({where:{user_id:req.body.user_id, resource_id: req.body.resource_id}}))
+    .then(() => UserResource.find({where:{user_id:req.query.user_id, resource_id: req.query.resource_id}}))
     .then(result => {
       res.send(result.active);
   }).catch(function (err) {
